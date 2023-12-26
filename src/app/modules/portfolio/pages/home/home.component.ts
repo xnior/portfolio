575bfa72362b9e62fc466a0125d35e95e5498eb8
Adhemar Jr <adhemar.machado@gmail.com>
2023-12-26 11:03:03 -0300

    Update home component with loading spinner

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 import { ExperiencesComponent } from '../../components/experiences/experiences.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnowledgeComponent, ExperiencesComponent, ProjectsComponent ],
+  imports: [HeaderComponent, KnowledgeComponent, ExperiencesComponent, ProjectsComponent, MatProgressSpinnerModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
