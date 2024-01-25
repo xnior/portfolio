2dca41776b4c81d74a60a43f180c9ea3d760e90a
Adhemar Jr <adhemar.machado@gmail.com>
2024-01-25 11:04:38 -0300

    Updates

---
+++ assets/DB/projects.ts
@@ -0,0 +1,137 @@
+export const arrayProjects = [
+  {
+    src: "assets/img/projects/img_1.jpg",
+    alt: "VFull",
+    title: "VFull",
+    width: "160px",
+    height: "160px",
+    description:
+      "VFull é uma plataforma de streaming de vídeo que permite aos usuários criar e compartilhar conteúdo ao vivo.",
+    links: [
+      {
+        name: "Ver projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_2.jpg",
+    alt: "Angular",
+    title: "Angular",
+    width: "160px",
+    height: "160px",
+    description:
+      "Angular é um sistema de design de código aberto para criar aplicativos da Web.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_3.jpg",
+    alt: "CSS",
+    title: "CSS",
+    width: "160px",
+    height: "160px",
+    description:
+      "CSS é um código de linguagem de estilo usado para descrever a apresentação de um documento escrito em uma linguagem de marcação.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_4.jpg",
+    alt: "SCSS",
+    title: "SCSS",
+    width: "160px",
+    height: "160px",
+    description:
+      "SCSS é uma linguagem de folha de estilo que é compilada em CSS. SCSS é a versão mais recente do SASS (Syntactically Awesome Style Sheets).",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_5.jpg",
+    alt: "HTML5",
+    title: "HTML5",
+    width: "160px",
+    height: "160px",
+    description:
+      "HTML5 é uma linguagem de marcação usada para estruturar e apresentar conteúdo na World Wide Web. É a quinta e última versão principal da tecnologia HTML.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_6.jpg",
+    alt: "Javascript",
+    title: "Javascipt",
+    width: "160px",
+    height: "160px",
+    description:
+      "Javascript desenvolveu-se a partir de uma ideia simples: trazer a programação para o navegador do usuário, reduzindo a dependência de aplicativos da Web do servidor.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_7.jpg",
+    alt: "Aviação",
+    title: "Aviação",
+    width: "160px",
+    height: "160px",
+    description:
+      "Projeto de serviços da aviação para instalações aeronáuticas.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_8.jpg",
+    alt: "Main frame Core Airplane",
+    title: "Main frame Core Airplane",
+    width: "160px",
+    height: "160px",
+    description:
+      "Melhorias apresentadas ao Main frame core em aeronaves para aplicação em segurança.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+  {
+    src: "assets/img/projects/img_9.jpg",
+    alt: "Apresentação de Service Bulletins para FAA e ANAC",
+    title: "ReactJS",
+    width: "160px",
+    height: "160px",
+    description:
+      "Services Bulletins com aplicação a aviões em helicopteros, que foram apresentados problemas significativos que geraram Bulletins, do total foram 13 SB's aprovados por minha análise da Engenharia Aeronáutica.",
+    links: [
+      {
+        name: "Ver Projeto",
+        href: "#",
+      },
+    ],
+  },
+];
