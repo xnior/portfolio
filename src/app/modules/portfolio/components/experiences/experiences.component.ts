8efed066f0ca7cf3edc60f762fdbd1f53527b750
Adhemar Jr <adhemar.machado@gmail.com>
2024-01-25 11:04:04 -0300

    Update .gitignore and code files

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -14,9 +14,38 @@ export class ExperiencesComponent {
       strong: 'Systems Engineer',
       p: 'Rockwell Collins Aviation',
       date: '2020 - Present',
-      description: 'I work as an Engineer in the company Rockwell Collins Aviation, where I have been able to learn and improve my skills in the development of web applications with Angular, React and Vue, as well as in the development of systems with Java, C++, C#.',
+      description: 'Trabalhei como Engenheiro em uma subsidiária da Rockwell Collins Aviation, onde pude aprender e aprimorar minhas habilidades no desenvolvimento de aplicações web com Angular, React e Vue, bem como no desenvolvimento de sistemas com Java, C++, C#.',
       logo: 'assets/img/sofka.png'
-    },text: 'I work as an Engineer in the company Rockwell Collins Aviation, where I have been able to learn and improve my skills in the development of web applications with Angular, React and Vue, as well as in the development of systems with Java, C++, C#.'
+    },text: 'Trabalhei como Engenheiro em uma subsidiária da Rockwell Collins Aviation, onde pude aprender e aprimorar minhas habilidades no desenvolvimento de aplicações web com Angular, React e Vue, bem como no desenvolvimento de sistemas com Java, C++, C#.',
+    
+    
+  },{
+    //create a new object relating for Aeronautical Engineer and an airplane pilot, Embry Riddle Aeronautical University, Daytona Beach, Florida, USA.
+    summary:{
+      strong: 'Aeronautical Engineer',
+      p: 'Embry Riddle Aeronautical University',
+      date: '2012 - 2017',
+      description: 'Concluir a licenciatura em Engenharia Aeronáutica foi um percurso verdadeiramente fascinante e desafiante, que moldou não só a minha carreira, mas também a minha perspectiva sobre a inovação na indústria aeroespacial. Ao longo do caminho, estive imerso num ambiente educativo que combinava o rigor académico com experiências práticas, proporcionando uma compreensão abrangente da complexidade e da evolução emocionante do setor.',
+      logo: 'assets/img/erau.png'
+    },text: 'Concluir a licenciatura em Engenharia Aeronáutica foi um percurso verdadeiramente fascinante e desafiante, que moldou não só a minha carreira, mas também a minha perspectiva sobre a inovação na indústria aeroespacial. Ao longo do caminho, estive imerso num ambiente educativo que combinava o rigor académico com experiências práticas, proporcionando uma compreensão abrangente da complexidade e da evolução emocionante do setor.',
+  },{
+    //create a new object relating for aeronautical studies and development of hardware and software for airplanes like Boeing Honeywell Aerospace like a research.
+    summary:{
+      strong: 'Projeto de Pesquisa Engenharia Aeronáutica',
+      p: 'Boeing, Honeywell Aerospace',
+      date: '2014 - 2016',
+      description: 'Participar do projeto de pesquisa estrutural aeronáutica da Boeing e da Honeywell Aerospace foi uma experiência excepcional que transcendeu os limites da sala de aula. Como estudante envolvido neste emocionante empreendimento, estive imerso em um ambiente colaborativo e inovador, trabalhando lado a lado com profissionais renomados e colegas igualmente apaixonados pela engenharia aeroespacial. Contribuir para a busca de soluções avançadas em estruturas aeronáuticas, utilizando métodos de análise de ponta e explorando materiais de última geração, me proporcionou uma visão única sobre o processo de desenvolvimento tecnológico da indústria. Ver as nossas ideias e esforços integrados no projeto global, liderado por duas potências como a Boeing e a Honeywell, não só elevou a minha compreensão prática, mas também alimentou a minha aspiração pela inovação. Esta oportunidade moldou não só o meu conhecimento técnico, mas também o meu entusiasmo em contribuir para o futuro da aviação.',
+      logo: 'assets/img/honeywell.png'
+    },text: 'Participar do projeto de pesquisa estrutural aeronáutica da Boeing e da Honeywell Aerospace foi uma experiência excepcional que transcendeu os limites da sala de aula. Como estudante envolvido neste emocionante empreendimento, estive imerso em um ambiente colaborativo e inovador, trabalhando lado a lado com profissionais renomados e colegas igualmente apaixonados pela engenharia aeroespacial. Contribuir para a busca de soluções avançadas em estruturas aeronáuticas, utilizando métodos de análise de ponta e explorando materiais de última geração, me proporcionou uma visão única sobre o processo de desenvolvimento tecnológico da indústria. Ver as nossas ideias e esforços integrados no projeto global, liderado por duas potências como a Boeing e a Honeywell, não só elevou a minha compreensão prática, mas também alimentou a minha aspiração pela inovação. Esta oportunidade moldou não só o meu conhecimento técnico, mas também o meu entusiasmo em contribuir para o futuro da aviação.',
+  },{
+    //create the description about my other course in the university in Brasil the name is UNIUBE and the course is agribusiness.
+    summary:{
+      strong: 'Agribusiness',
+      p: 'UNIUBE',
+      date: '2004 - 2007',
+      description: 'Como aluno concluinte do curso de Gestão do Agronegócio, posso afirmar que a experiência foi enriquecedora e de grande relevância para minha trajetória profissional. Durante o programa, estive imerso em um ambiente educacional que equilibra teoria e prática, proporcionando uma compreensão abrangente dos desafios e oportunidades do setor agrícola brasileiro.',
+      logo: 'assets/img/uniube.png'
+    },text: 'Como aluno concluinte do curso de Gestão do Agronegócio, posso afirmar que a experiência foi enriquecedora e de grande relevância para minha trajetória profissional. Durante o programa, estive imerso em um ambiente educacional que equilibra teoria e prática, proporcionando uma compreensão abrangente dos desafios e oportunidades do setor agrícola brasileiro.',
   }
 ])
 
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,112 +1,36 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, OnInit, inject, signal } from "@angular/core";
 import { IProject } from "../../interface/IProject.iterface";
 import { MatDialog } from "@angular/material/dialog";
 
 import { DialogProjectsComponent } from "../dialog/dialog-projects/dialog-projects.component";
 import { MatButtonModule } from "@angular/material/button";
 import { EDialogPanelClass } from "../../enum/EDialogPanelClass.enum";
+import { arrayProjects } from "../../../../../assets/DB/projects";
+import { CommonModule, I18nPluralPipe } from "@angular/common";
+import { Observable, delay, of } from "rxjs";
+import { ApiService } from "../../../services/api.service";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
   selector: "app-projects",
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: "./projects.component.html",
   styleUrl: "./projects.component.scss",
 })
-export class ProjectsComponent {
-  constructor(public dialog: MatDialog) {}
-  public arrayProjects = signal<IProject[]>([
-    {
-      src: "assets/img/projects/img_1.jpg",
-      alt: "VFull",
-      title: "VFull",
-      width: "160px",
-      height: "160px",
-      description:
-        "VFull é uma plataforma de streaming de vídeo que permite aos usuários criar e compartilhar conteúdo ao vivo.",
-      links: [
-        {
-          name: "Ver projeto",
-          href: "#",
-        },
-      ],
-    },
-    {
-      src: "assets/img/projects/img_2.jpg",
-      alt: "Angular",
-      title: "Angular",
-      width: "160px",
-      height: "160px",
-      description:
-        "Angular é um sistema de design de código aberto para criar aplicativos da Web.",
-      links: [
-        {
-          name: "Ver Projeto",
-          href: "#",
-        },
-      ],
-    },
-    {
-      src: "assets/img/projects/img_3.jpg",
-      alt: "CSS",
-      title: "CSS",
-      width: "160px",
-      height: "160px",
-      description:
-        "CSS é um código de linguagem de estilo usado para descrever a apresentação de um documento escrito em uma linguagem de marcação.",
-      links: [
-        {
-          name: "Ver Projeto",
-          href: "#",
-        },
-      ],
-    },
-    {
-      src: "assets/img/projects/img_4.jpg",
-      alt: "SCSS",
-      title: "SCSS",
-      width: "160px",
-      height: "160px",
-      description:
-        "SCSS é uma linguagem de folha de estilo que é compilada em CSS. SCSS é a versão mais recente do SASS (Syntactically Awesome Style Sheets).",
-      links: [
-        {
-          name: "Ver Projeto",
-          href: "#",
-        },
-      ],
-    },
-    {
-      src: "assets/img/projects/img_5.jpg",
-      alt: "HTML5",
-      title: "HTML5",
-      width: "160px",
-      height: "160px",
-      description:
-        "HTML5 é uma linguagem de marcação usada para estruturar e apresentar conteúdo na World Wide Web. É a quinta e última versão principal da tecnologia HTML.",
-      links: [
-        {
-          name: "Ver Projeto",
-          href: "#",
-        },
-      ],
-    },
-    {
-      src: "assets/img/projects/img_6.jpg",
-      alt: "Javascript",
-      title: "Javascipt",
-      width: "160px",
-      height: "160px",
-      description:
-        "Javascript desenvolveu-se a partir de uma ideia simples: trazer a programação para o navegador do usuário, reduzindo a dependência de aplicativos da Web do servidor.",
-      links: [
-        {
-          name: "Ver Projeto",
-          href: "#",
-        },
-      ],
-    },
-  ]);
+export class ProjectsComponent implements OnInit {
+  public dialog = inject(MatDialog);
+  #apiService = inject(ApiService);
+  ngOnInit(): void {
+    this.loadArray$();
+  }
+  public loadArray:IProject[]=[];
+
+public async loadArray$(): Promise<void>{
+  this.loadArray = await this.#apiService.loadArray$(arrayProjects);
+}
+
+
   public openDialog(data: IProject) {
     this.dialog.open(DialogProjectsComponent, {
       data,
