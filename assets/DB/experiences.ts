52bf8bc597e3018999ef28e2be61351ad1c1affc
Adhemar Jr <adhemar.machado@gmail.com>
2024-01-28 18:13:18 -0300

    Updates

---
+++ assets/DB/experiences.ts
@@ -0,0 +1,66 @@
+export const arrayExperiences = [{
+    summary:{
+      strong: 'Systems Engineer',
+      p: 'Projeto - Rockwell Collins Aviation',
+      date: '2021 - 2021',
+      description: 'Trabalhei como Engenheiro em uma subsidiária da Rockwell Collins Aviation, onde pude aprender e aprimorar minhas habilidades no desenvolvimento de aplicações web com Angular, React e Vue, bem como no desenvolvimento de sistemas com Java, C++, C#.',
+      logo: 'assets/img/sofka.png'
+    },text: 'Trabalhei como Engenheiro em uma subsidiária da Rockwell Collins Aviation, onde pude aprender e aprimorar minhas habilidades no desenvolvimento de aplicações web com Angular, React e Vue, bem como no desenvolvimento de sistemas com Java, C++, C#.',
+    
+    
+  },{
+    //create a new object relating for Aeronautical Engineer and an airplane pilot, Embry Riddle Aeronautical University, Daytona Beach, Florida, USA.
+    summary:{
+      strong: 'Engenharia Aeronáutica (Convalidação)',
+      p: 'Embry Riddle Aeronautical University',
+      date: '2012 - 2017',
+      description: 'Concluir o bacharelado em Engenharia Aeronáutica foi um percurso verdadeiramente fascinante e desafiante, que moldou não só a minha carreira, mas também a minha perspectiva sobre a inovação na indústria aeroespacial. Ao longo do caminho, estive imerso num ambiente educativo que combinava o rigor académico com experiências práticas, proporcionando uma compreensão abrangente da complexidade e da evolução emocionante do setor.',
+      logo: 'assets/img/erau.png'
+    },text: 'Concluir o bacharelado em Engenharia Aeronáutica foi um percurso verdadeiramente fascinante e desafiante, que moldou não só a minha carreira, mas também a minha perspectiva sobre a inovação na indústria aeroespacial. Ao longo do caminho, estive imerso num ambiente educativo que combinava o rigor académico com experiências práticas, proporcionando uma compreensão abrangente da complexidade e da evolução emocionante do setor.',
+  },{
+    //create a new object relating for aeronautical studies and development of hardware and software for airplanes like Boeing Honeywell Aerospace like a research.
+    summary:{
+      strong: 'Projeto de Pesquisa Engenharia Aeronáutica',
+      p: 'Boeing, Honeywell Aerospace',
+      date: '2014 - 2016',
+      description: 'Participar do projeto de pesquisa estrutural aeronáutica da Boeing e da Honeywell Aerospace foi uma experiência excepcional que transcendeu os limites da sala de aula. Como estudante envolvido neste emocionante empreendimento, estive imerso em um ambiente colaborativo e inovador, trabalhando lado a lado com profissionais renomados e colegas igualmente apaixonados pela engenharia aeroespacial. Contribuir para a busca de soluções avançadas em estruturas aeronáuticas, utilizando métodos de análise de ponta e explorando materiais de última geração, me proporcionou uma visão única sobre o processo de desenvolvimento tecnológico da indústria. Ver as nossas ideias e esforços integrados no projeto global, liderado por duas potências como a Boeing e a Honeywell, não só elevou a minha compreensão prática, mas também alimentou a minha aspiração pela inovação. Esta oportunidade moldou não só o meu conhecimento técnico, mas também o meu entusiasmo em contribuir para o futuro da aviação.',
+      logo: 'assets/img/honeywell.png'
+    },text: 'Participar do projeto de pesquisa estrutural aeronáutica da Boeing e da Honeywell Aerospace foi uma experiência excepcional que transcendeu os limites da sala de aula. Como estudante envolvido neste emocionante empreendimento, estive imerso em um ambiente colaborativo e inovador, trabalhando lado a lado com profissionais renomados e colegas igualmente apaixonados pela engenharia aeroespacial. Contribuir para a busca de soluções avançadas em estruturas aeronáuticas, utilizando métodos de análise de ponta e explorando materiais de última geração, me proporcionou uma visão única sobre o processo de desenvolvimento tecnológico da indústria. Ver as nossas ideias e esforços integrados no projeto global, liderado por duas potências como a Boeing e a Honeywell, não só elevou a minha compreensão prática, mas também alimentou a minha aspiração pela inovação. Esta oportunidade moldou não só o meu conhecimento técnico, mas também o meu entusiasmo em contribuir para o futuro da aviação.',
+  },{
+    //create the description about my other course in the university in Brasil the name is UNIUBE and the course is agribusiness.
+    summary:{
+      strong: 'Agribusiness',
+      p: 'UNIUBE',
+      date: '2004 - 2007',
+      description: 'Como aluno concluinte do curso de Gestão do Agronegócio, posso afirmar que a experiência foi enriquecedora e de grande relevância para minha trajetória profissional. Durante o programa, estive imerso em um ambiente educacional que equilibra teoria e prática, proporcionando uma compreensão abrangente dos desafios e oportunidades do setor agrícola brasileiro.',
+      logo: 'assets/img/uniube.png'
+    },text: 'Como aluno concluinte do curso de Gestão do Agronegócio, posso afirmar que a experiência foi enriquecedora e de grande relevância para minha trajetória profissional. Durante o programa, estive imerso em um ambiente educacional que equilibra teoria e prática, proporcionando uma compreensão abrangente dos desafios e oportunidades do setor agrícola brasileiro.',
+  },{
+    //create for electrical engineering
+    summary:{
+      strong: 'Engenharia Elétrica',
+      p: 'UNIUBE',
+      date: '2017 - 2023',
+      description: 'Como aluno concluinte do curso de Engenharia Elétrica, posso afirmar que a experiência foi enriquecedora e de grande relevância para minha trajetória profissional. Durante o programa, estive imerso em um ambiente educacional que equilibra teoria e prática, proporcionando uma compreensão abrangente dos desafios e oportunidades do setor elétrico brasileiro.',
+      logo: 'assets/img/uniube.png'
+    },text: 'Como aluno concluinte do curso de Engenharia Elétrica, posso afirmar que a experiência foi enriquecedora e de grande relevância para minha trajetória profissional. Durante o programa, estive imerso em um ambiente educacional que equilibra teoria e prática, proporcionando uma compreensão abrangente dos desafios e oportunidades do setor elétrico brasileiro.',
+  },{
+    //faça um também destacando que fiz projetos para aviação ANAC
+    summary:{
+      strong: 'Credênciado e Habilitado ANAC',
+      p: 'ANAC Brasil',
+      date: '2008(Piloto) - 2017(Engenheiro) - presente',
+      description: 'Como Engenheiro efetuo trabalhos juntamente com aprovação a Agência ANAC como manutenção e troca de equipamentos de técnologia Analógica e Digital(predominante), também sou Piloto de Avião e Helicóptero possuo mais de 3983 horas de vôo em avião dentre elas de Mono Motor, Multi Motor, Turbo Hélice King Air C90 e Jatos, para jatos a maior experiência é em Citation e Gulfstream G200, para Helicóptero possuo habilitação Mono Motor e Multi Motor com mais experiência em Agusta 109.',
+      logo: 'assets/img/anac.png'
+          },text: 'Após experiências de várias serviços como projetos e aprovações, atualmente atuo como Engenheiro Aeronáutico e Eletricista e também como Piloto, desempenhando atividades em colaboração com a aprovação da Agência Nacional de Aviação Civil (ANAC).',
+  },{
+    //faça um texto semelhante ao da ANAC porém para FAA Estados Unidos
+    summary:{
+      strong: 'Credênciado FAA',
+      p: 'FAA Estados Unidos',
+      date: '2014(Piloto e Engenheiro) - presente',
+      description: 'Como Engenheiro efetuo trabalhos juntamente com aprovação a Agência FAA.',
+      logo: 'assets/img/faa.png'
+    },text: 'Após experiências de várias serviços como projetos e aprovações, atualmente atuo como Engenheiro Aeronáutico e Eletricista e também como Piloto, desempenhando atividades em colaboração com a aprovação da Federal Aviation Administration (FAA).',
+  }
+]
