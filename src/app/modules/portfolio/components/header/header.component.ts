3e2f927e6f8102d030f74c942d8db61a3fb1cc5c
Adhemar Jr <adhemar.machado@gmail.com>
2024-01-28 13:24:51 -0300

    Add menu with curriculum information

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
