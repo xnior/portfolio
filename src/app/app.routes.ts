3c29f4f36e1b13cfa50fd48811c4b6d6aaaa3a8a
Adhemar Jr <adhemar.machado@gmail.com>
2024-01-28 17:53:02 -0300

    Add dynamic route for unknown paths and add animation to projects component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,10 @@ import { HomeComponent } from './modules/portfolio/pages/home/home.component';
 
 export const routes: Routes = [{
     path:'',
-    component: HomeComponent
+    component: HomeComponent,
+},{
+    path: '**',
+    loadComponent() {
+        return import('./modules/portfolio/pages/home/home.component').then(m => m.HomeComponent);
+    },
 }];
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -8,14 +8,31 @@ import { EDialogPanelClass } from "../../enum/EDialogPanelClass.enum";
 import { CommonModule } from "@angular/common";
 import { Observable, delay, of, tap } from "rxjs";
 import { ApiService } from "../../../services/api.service";
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { animate, group, query, stagger, state, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: "app-projects",
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatProgressBarModule],
+  imports: [CommonModule, MatButtonModule, MatProgressBarModule, BrowserModule],
   templateUrl: "./projects.component.html",
   styleUrl: "./projects.component.scss",
+  animations: [
+    trigger('anim', [
+      transition('*=>*', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-30px)' }),
+          stagger(100, [
+            group([
+              animate('1s ease-in-out', style({ opacity: 1 })),
+              animate('1s ease-in-out', style({ transform: 'none' })),
+            ]),
+          ]),
+        ], { optional: true }),
+      ]),
+    ]),
+  ]
 })
 export class ProjectsComponent implements OnInit {
   public dialog = inject(MatDialog);
@@ -23,16 +40,16 @@ export class ProjectsComponent implements OnInit {
 
   // public loadArray$(): Observable<any[]>{return this.#apiService.readApiService$;};
 
-  public arrayLoad:IProject[]=[]
-  async ngOnInit():Promise<any> {
+  public arrayLoad: IProject[] = []
+  async ngOnInit(): Promise<any> {
 
     // this.#apiService.readApiService$.subscribe();
-    
+
     this.arrayLoad = await this.#apiService.progressiveRead();
-      // console.log(this.arrayLoad);
+    // console.log(this.arrayLoad);
   }
-  
-  
+
+
   public openDialog(data: IProject) {
     this.dialog.open(DialogProjectsComponent, {
       data,
