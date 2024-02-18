1f7960bed9f6a83791ba1e75a16dcf5cd2e0bb71
Adhemar Jr <adhemar.machado@gmail.com>
2024-02-17 22:42:43 -0300

    Refactor header component HTML and SCSS files

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { loadingAnimation } from '../../../../animations/animation';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +12,56 @@ import { loadingAnimation } from '../../../../animations/animation';
   imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  animations:[loadingAnimation]
+  animations: [loadingAnimation]
 })
 
 export class HeaderComponent {
+  public className = 'blur';
+  public classNameEmail = 'blur';
+  timerTel: number  = 0;
+  public classTimerTel = 'hidden';
+  timerEmail: number  = 0;
+  public classTimerEmail = 'hidden';
 
+  public reversiveTelTimer() {
+    this.timerTel = 5;
+    this.classTimerTel = 'visible';
+    const timer = setInterval(() => {
+      this.timerTel! -= 0.1;
+      if (this.timerTel! <= 0.3) {
+        clearInterval(timer);
+        this.classTimerTel = 'hidden';
+      }
+    }, 100);
+  }
+
+  public reversiveEmailTimer() {
+
+    this.timerEmail = 5;
+    this.classTimerEmail = 'visible';
+    const timer = setInterval(() => {
+      this.timerEmail! -= 0.1;
+      if (this.timerEmail! <= 0.3) {
+        clearInterval(timer);
+        this.classTimerEmail = 'hidden';
+      }
+    }, 100);
+  }
+
+  public changeClass() {
+
+    if (this.className == 'blur') {
+      this.className = 'unblur';
+      setTimeout(() => this.className = 'blur', 5000);
+      this.reversiveTelTimer();
+    }
+  }
+  public changeClassEmail() {
+
+    if (this.classNameEmail == 'blur') {
+      this.classNameEmail = 'unblur';
+      setTimeout(() => this.classNameEmail = 'blur', 5000);
+      this.reversiveEmailTimer();
+    }
+  }
 }
