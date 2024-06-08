fc1a34004ef562e06f0093d370ce82eb33faa483
Adhemar Jr <adhemar.machado@gmail.com>
2024-06-08 10:12:29 -0300

    Initial Commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'PORTFOLIO' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('PORTFOLIO');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PORTFOLIO');
+  });
+});
---
+++ src/app/modules/portfolio/interface/IExperience.interface.ts
@@ -0,0 +1,10 @@
+export interface IExperience {
+    summary: {
+        strong: string;
+        p: string;
+        date: string;
+        description: string;
+        logo: string;
+    };
+    text: string;
+}
